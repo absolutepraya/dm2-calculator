7367eca86d525f88a166defc70211030ce333e49
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-02-08 21:59:22 +0700

    first calcu: modular exp

---
+++ Modular_Exponentiation.py
@@ -0,0 +1,78 @@
+# Version: 1.0
+
+print("""┳┳┓   ┓  ┓      ┏┓             •   •    
+┃┃┃┏┓┏┫┓┏┃┏┓┏┓  ┣ ┓┏┏┓┏┓┏┓┏┓┏┓╋┓┏┓╋┓┏┓┏┓
+┛ ┗┗┛┗┻┗┻┗┗┻┛   ┗┛┛┗┣┛┗┛┛┗┗ ┛┗┗┗┗┻┗┗┗┛┛┗  by absolutepraya
+                    ┛
+
+This program not just calculates the modular exponentiation
+of a number, but also shows the steps of the calculation. 
+The steps are shown in a table format.
+
+————————————————————————————————————————————————————————————
+""")
+
+# Input
+print("Enter the base, exponent, and mod of the equation.")
+base = int(input("Base     =  "))
+exponent = int(input("Exponent =  "))
+mod = int(input("Mod      =  "))
+print("""
+————————————————————————————————————————————————————————————
+""")
+
+# Power calculator
+exp_bin = bin(exponent)[2:]
+exp_bin_len = len(exp_bin)
+pow_lst = [base % mod]
+pow_str = []
+for i in range(exp_bin_len - 1):
+    pow_lst.append(pow_lst[i] ** 2 % mod)
+    pow_str.append(f"{pow_lst[i]}^2 % {mod} = {pow_lst[i]**2} % {mod} = {pow_lst[i + 1]}")
+
+# Result calculator
+exp_bin_rev = exp_bin[::-1]
+res_lst = []
+res_str = []
+for i in range(exp_bin_len):
+    if exp_bin_rev[i] == "1":
+        if len(res_lst) == 0:
+            res_lst.append((1 * pow_lst[i]) % mod)
+            res_str.append(f"1 * {pow_lst[i]} % {mod} = {res_lst[i]}")
+        else:
+            res_lst.append((res_lst[-1] * pow_lst[i]) % mod)
+            res_str.append(f"{res_lst[-2]} * {pow_lst[i]} % {mod} = {res_lst[i]}")
+    else:
+        if len(res_lst) == 0:
+            res_lst.append(1)
+        else:
+            res_lst.append(res_lst[-1])
+        res_str.append(f"{res_lst[i]}")
+
+# Tidying up the lists for the table
+pow_lst = pow_lst[1:]
+pow_lst.append("-")
+pow_str.append("-")
+
+# Print initialization
+print(f"""### INITIALIZATION
+Exponent in binary: {exp_bin}
+Initial result:     1
+Initial power:      {base} % {mod} = {base % mod}
+
+### CALCULATION
+""")
+
+# Create a header for the table
+header = ['Bit', 'Result', 'Calculate Power', 'Power']
+column_widths = [5, 25, 40, 7]
+
+# Print the header and the separator
+print('|'.join(f'{title:^{width}}' for title, width in zip(header, column_widths)))
+print('—' * (sum(column_widths) + len(column_widths) - 1))
+
+# Iterate over the lists simultaneously and print rows
+for row in zip(list(exp_bin_rev), res_str, pow_str, pow_lst):
+    print('|'.join(f'{str(item):^{width}}' for item, width in zip(row, column_widths)))
+
+print(f"\nThe result is {res_lst[i]}")
