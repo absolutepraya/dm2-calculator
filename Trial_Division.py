c48506ef96b6360f82774d42fa215fea347dfe6e
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-02-09 11:03:28 +0700

    v1.2: ceil -> floor rounding

--- Trial_Division.py
+++ Trial_Division.py
@@ -1,4 +1,4 @@
-# Version: 1.0
+# Version: 1.2
 
 import math
 import sys
@@ -20,23 +20,23 @@ if len(sys.argv) > 1:
 # Input inside the program
 else:
     print("Enter a number.")
-    num = int(input("Number =  "))
+    num = int(input("Number = "))
 print("""
 ————————————————————————————————————————————————————————————
 """)
 
 # Prime numbers list using Sieve of Eratosthenes algorithm
-num_root = math.ceil(num**0.5)
+num_root = math.floor(num**0.5)
 sieve = [True] * (num_root+1)
 for x in range(2, int(num_root**0.5) + 1):
     if sieve[x]: sieve[2*x::x] = [False] * len(sieve[2*x::x])
 prime_lst =  [i for i in range(2, num_root+1) if sieve[i]]
 
 # Result (1 of 2)
-print(f"""SOLVING STEPS:
+print(f"""### SOLVING STEPS:
 
-(1) Find the square root of the number, then round it up.
-x = ⌈√{num}⌉ = ⌈{num**0.5}⌉ = {num_root}
+(1) Find the square root of the number, then round it down.
+x = ⌊√{num}⌋ = ⌊{num**0.5}⌋ = {num_root}
 
 (2) Create a list of prime numbers from 2 to x.
 Primes = {', '.join(map(str, prime_lst))}
@@ -57,8 +57,12 @@ for prime_num in prime_lst:
         print(f"Try {tries}: {num} % {prime_num} = {mod_res} —> {prime_num} ∤ {num} (not divisible)")
 
 # Result (2 of 2)
-print(f"\nTotal of tries: {tries}")
+print(f"""
+Number of tries needed: {tries}
+Maximum number of tries: {len(prime_lst)}
+""")
 if prime:
-    print(f"∴ {num} is a prime number, it's not divisible by any of the prime numbers from 2 to {num_root}.")
+    print(f"""∴ {num} is a prime number.
+  It's not divisible by any of the prime numbers from 2 to {num_root}.""")
 else:
     print(f"∴ {num} is NOT a prime number, it's divisible by {prime_num}.")
