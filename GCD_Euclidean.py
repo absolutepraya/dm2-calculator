03b036d1757f15b64911e48e376128ee36aff7bc
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-02-13 09:32:17 +0700

    v1.20: visual update

--- GCD_Euclidean.py
+++ GCD_Euclidean.py
@@ -1,16 +1,17 @@
-# Version: 1.0
+# Version: 1.20
 
 import sys
 
-print("""┏┓   ┓• ┓        ┏┓┏┓┳┓
+print("""\033[32m┏┓   ┓• ┓        ┏┓┏┓┳┓
 ┣ ┓┏┏┃┓┏┫┏┓┏┓┏┓  ┃┓┃ ┃┃
-┗┛┗┻┗┗┗┗┻┗ ┗┻┛┗  ┗┛┗┛┻┛  by absolutepraya
+┗┛┗┻┗┗┗┗┻┗ ┗┻┛┗  ┗┛┗┛┻┛  by absolutepraya\033[0m
 
 Shows the steps of the Euclidean algorithm to find the GCD 
 of two numbers.
 
-————————————————————————————————————————————————————————————
-""")
+———— I N P U T ————————————————————————————————————————————————————————
+
+Enter two numbers for GCD(x, y).""")
 
 # Command line arguments
 if len(sys.argv) > 1:
@@ -18,15 +19,13 @@ if len(sys.argv) > 1:
     num2 = int(sys.argv[2])
 # Input inside the program
 else:
-    print("Enter two numbers for GCD(x, y).")
     num1 = int(input("x = "))
     num2 = int(input("y = "))
-print(f"""GCD({num1}, {num2})
+print(f"""\033[33mGCD({num1}, {num2})\033[0m
 
-————————————————————————————————————————————————————————————
+———— S T E P S ————————————————————————————————————————————————————————
 
 ### SOLVING STEPS
-
 """)
 
 # Euclidean algorithm
@@ -36,4 +35,4 @@ while y != 0:
     print(f"{x} = {y} * {x // y} + {x % y}")
     x, y = y, x % y
 
-print(f"\n∴ Therefore, GCD({num1}, {num2}) = {x}")
+print(f"\n\033[33m∴ Therefore, GCD({num1}, {num2}) = {x}\033[0m")
